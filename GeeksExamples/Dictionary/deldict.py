1fcdf30fc43a1de7cc2fa3126a715abe52c6439b
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-16 21:29:04 +0530

    delete a dictionary or clear a dict

---
+++ GeeksExamples/Dictionary/deldict.py
@@ -0,0 +1,18 @@
+# How to delete a dictionary
+
+test = {"Arushi": 22, "Mani": 21, "Haritha": 20}
+print(test)
+del test
+#print(test)
+
+
+
+ 
+ 
+# Delete all keys 
+test = {"Arushi": 22, "Mani": 21, "Haritha": 20}
+print(len(test))
+print(test)
+test.clear()
+print(len(test))
+print(test)
