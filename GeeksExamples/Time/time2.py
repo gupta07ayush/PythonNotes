2339cf1ca6a6a68675c0aa4cb0c80b340cd13e68
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-22 08:04:50 +0530

    using time module

--- GeeksExamples/Time/time2.py
+++ GeeksExamples/Time/time2.py
@@ -8,3 +8,26 @@ print(now)
 # strftime() method is used to create a string representing the current time.
 only_time = now.strftime("%H:%M:%S")
 print(only_time)
+
+
+
+# Method 2
+
+from datetime import datetime
+ 
+# Time object containing
+# the current time.
+time = datetime.now().time()
+print("Current Time =", time)
+
+
+
+
+
+# Method 3 using time module
+import time
+x = time.localtime()
+print(x)
+
+currentTime = time.strftime("%H:%M:%S")
+print(currentTime)
