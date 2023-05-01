557d683a2f9144670249d7fe466cb43bde680b23
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-01 20:34:07 +0530

    no of occurance key value pair

---
+++ GeeksExamples/File Handling/11count.py
@@ -0,0 +1,25 @@
+# Python Program to count the number of occurrence of a key-value pair in a text file.
+
+f = open('new.txt', 'r')
+d = dict()
+
+for res in f:
+    # removing new line and extra space characters
+    res = res.strip()
+    res = res.lower()
+
+    # separating key-value pairs
+    lines = res.split()
+
+    for line in lines:
+        if line in d:
+            # if the key-value pair is present in d then increment its value by one
+            d[line] = d[line]+1
+        else:
+            # insert the key-value pair in the dictionary and sets its value to one
+            d[line] = 1
+f.close()
+
+# printing result
+for key in list(d.keys()):
+    print("The count of {} is {}".format(key, d[key]))
