2a934c4dfc31a82c40170d8c4606583c99e3a864
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-28 20:00:16 +0530

    pytest test_second.py -k login

---
+++ TestingTutorials/test_second.py
@@ -0,0 +1,17 @@
+# pytest will run all files from current dir/ subdirectory.
+# pytest test_second.py -This will run only specified file
+
+def test_login():
+    print("Login to application")
+
+
+def test_checkout():
+    print("Checkout")
+
+
+def test_logout():
+    print("Log out from the application")
+
+
+# Note: This will run only first function of the this file.
+# pytest test_second.py -k login
