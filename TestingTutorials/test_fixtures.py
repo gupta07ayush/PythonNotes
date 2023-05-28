1f03ac2909836179b2e76f89d1d24997450c0ae6
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-29 01:37:21 +0530

    fixtures1

---
+++ TestingTutorials/test_fixtures.py
@@ -0,0 +1,22 @@
+import pytest
+
+# here first lines are repeating multiple times
+# so we will create fixtures for this in test_fixtures2.py
+
+
+def test_1():
+    print("Start the browser")
+    print("Test 1 executed")
+    print("Close the browser")
+
+
+def test_2():
+    print("Start the browser")
+    print("Test 2 executed")
+    print("Close the browser")
+
+
+def test_3():
+    print("Start the browser")
+    print("Test 3 executed")
+    print("Close the browser")
