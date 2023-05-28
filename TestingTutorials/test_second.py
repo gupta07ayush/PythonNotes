b541d810a9da9118d474493c24040735826d0a72
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-28 20:05:21 +0530

    -v verbose flag

--- TestingTutorials/test_second.py
+++ TestingTutorials/test_second.py
@@ -13,5 +13,9 @@ def test_logout():
     print("Log out from the application")
 
 
-# Note: This will run only first function of the this file.
+# Note 1: This will run only first function of the this file.
 # pytest test_second.py -k login
+
+
+# Note 2: v=verbose -It will show you some more details
+# pytest -rA -v
