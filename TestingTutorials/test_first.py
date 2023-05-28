85094a1659dcf328059196377466f42ec826b3f0
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-28 19:41:19 +0530

    second test

--- TestingTutorials/test_first.py
+++ TestingTutorials/test_first.py
@@ -10,3 +10,14 @@ def test_1():
 
 # this test will fail because here x is not equal to y.
 # to run the test simply write pytest in the terminal.
+
+
+def test_2():
+    # I am writing another test.
+    x = 5
+    y = 5
+
+    assert x == y
+# this test will pass because 5=5
+# fail = F
+# pass = . (green dot)
