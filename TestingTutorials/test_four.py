09ee4249b9b84f0ebf3b74ffd7d6c213ab72714e
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-28 21:48:03 +0530

    pytest --html=myreport.html

--- TestingTutorials/test_four.py
+++ TestingTutorials/test_four.py
@@ -6,5 +6,9 @@ def test_responsecode200():
 def test_responsecode400():
     print("API is not working fine")
 
-# --junitxml="filename.xml"
+# pytest --junitxml="filename.xml"
 # this will generate and dump test results in filename
+
+
+# pytest --html=myreport.html
+# This will generate an html file and dump the results in it.
