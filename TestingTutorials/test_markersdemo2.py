ddbf68ae54e9fd78676687c1945aa93f571162df
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-29 00:17:41 +0530

    marker xfail

--- TestingTutorials/test_markersdemo2.py
+++ TestingTutorials/test_markersdemo2.py
@@ -7,10 +7,12 @@ def test_login():
     print("Login done")
 
 
-@pytest.mark.skipif(sys.version_info < (3.12), reason="Python version not supported")
+@pytest.mark.skipif(sys.version_info < (3, 12), reason="Python version not supported")
 def test_addProducts():
     print("added products")
 
 
+@pytest.mark.xfail
 def test_logout():
+    assert True
     print("Logout done")
