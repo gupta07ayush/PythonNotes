95e9db0127b19ae0f314558cdd10af0561210d93
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-28 21:43:38 +0530

    pytest --jnitxml=file.xml

---
+++ TestingTutorials/test_four.py
@@ -0,0 +1,10 @@
+
+def test_responsecode200():
+    print("API is working fine")
+
+
+def test_responsecode400():
+    print("API is not working fine")
+
+# --junitxml="filename.xml"
+# this will generate and dump test results in filename
