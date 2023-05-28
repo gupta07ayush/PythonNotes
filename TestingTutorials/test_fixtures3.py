d4205d54ea0e0dcd0a1162b732b376c8aaf690e2
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-29 01:57:57 +0530

    selenium webdriver fixture testing

---
+++ TestingTutorials/test_fixtures3.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+# here first lines are repeating multiple times
+# so we will create fixtures for this here
+
+
+driver = None
+
+
+@pytest.fixture
+def setup():
+    print("Start the browser")
+    global driver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield
+    driver.quit()
+    print("Close the browser")
+
+
+def test_1(setup):
+    driver.get('https://google.com')
+    print("Test 1 executed")
+
+
+def test_2(setup):
+    driver.get('https://github.com/gupta07ayush')
+    print("Test 2 executed")
+
+
+def test_3(setup):
+    driver.get('https://youtube.com')
+    print("Test 3 executed")
+
+
+# note: If you don't see the close the browser line which is after yield in the test results. use -s flag
+# pytest .\test_fixtures2.py -s
